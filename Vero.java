653fce3a637fea75843dd51f2c6635e0415c4e9a
Atherizz <saveroathallah86@gmail.com>
2024-12-02 16:35:20 +0700

    jadi coy

--- Vero.java
+++ Vero.java
@@ -4,9 +4,9 @@ public class Vero {
     static int counter = 0;
     public static void main(String[] args) {
 
-        String prestasi [][] = new String[100][5];
+        String prestasi [][] = new String[100][4];
+        int tahunPrestasi [] = new int [100];
         String kategori [] = {"Nama", "NIM", "Jenis", "Tingkat", "Tahun"};
-        
         
         Scanner sc = new Scanner(System.in);
         int menu;
@@ -25,14 +25,15 @@ public class Vero {
         switch (menu) {
             case 1:
             System.out.println("Tambah Data Prestasi");
-            inputPrestasi(prestasi, kategori);
+            inputPrestasi(prestasi, tahunPrestasi);
             break;
             case 2:
             System.out.println("Tampilkan Semua Prestasi");
-            showPrestasi(prestasi, kategori);
+            showPrestasi(prestasi, kategori, tahunPrestasi);
             break;   
             case 3:
             System.out.println("Analisis Prestasi");
+            analisisPrestasi(prestasi, kategori, tahunPrestasi);
             break;    
             case 4:
             System.out.println("Keluar");   
@@ -49,7 +50,7 @@ public class Vero {
 
     }
 
-    public static void inputPrestasi (String [][] prestasi, String [] kategori) {
+    public static void inputPrestasi (String [][] prestasi, int [] tahunPrestasi) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Masukkan nama :");
@@ -65,14 +66,45 @@ public class Vero {
         prestasi[counter][3] = sc.nextLine();
 
         System.out.print("Masukkan tahun :");
-        prestasi[counter][4] = sc.nextLine();
+        tahunPrestasi[counter] = sc.nextInt();
+
+        while (tahunPrestasi[counter] < 2010 || tahunPrestasi[counter] > 2024) {
+            System.out.println("Input tidak valid, coba lagi!");
+            System.out.print("Masukkan tahun :");
+            tahunPrestasi[counter] = sc.nextInt();
+        }
+
         counter++;
     }
 
-    public static void showPrestasi (String [][] prestasi, String[] kategori) {
+    public static void showPrestasi (String [][] prestasi, String[] kategori, int [] tahunPrestasi) {
+        System.out.println("=== DAFTAR SEMUA PRESTASI ===");
         for(int i = 0; i < counter; i++) {
             for (int j = 0; j < prestasi[0].length;j++) {
                     System.out.print(kategori[j] + " : " + prestasi[i][j] + " | ");
+            }
+                System.out.print("Tahun : " + tahunPrestasi[i] + " | ");
+        
+            System.out.println();
+        }
+    }
+
+    public static void analisisPrestasi (String [][] prestasi, String[] kategori, int [] tahunPrestasi) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan Jenis Prestasi yang ingin dianalsisis : ");
+        String jenisPrestasi = sc.nextLine();
+
+        for(int i = 0; i < counter; i++) {
+            for (int j = 0; j < prestasi[0].length; j++) {
+                if (j == 2) {
+                    continue;
+                }
+                if (prestasi[i][2].equalsIgnoreCase(jenisPrestasi)) {
+                    System.out.print(kategori[j] + " : " + prestasi[i][j] + " | ");
+                }
+            }
+            if (prestasi[i][2].equalsIgnoreCase(jenisPrestasi)) {
+                System.out.print("Tahun : " + tahunPrestasi[i]);
             }
             System.out.println();
         }
