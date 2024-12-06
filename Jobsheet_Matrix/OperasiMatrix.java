ab5b41e6b7110cfac72aeda19e52c7e3fd5ddb64
Atherizz <saveroathallah86@gmail.com>
2024-12-06 11:19:07 +0700

    fix printMatrix function

--- Jobsheet_Matrix/OperasiMatrix.java
+++ Jobsheet_Matrix/OperasiMatrix.java
@@ -72,9 +72,11 @@ public class OperasiMatrix {
 
 public static void printMatrix(int [][] matrix) {
     for(int i = 0; i < matrix.length; i++) {
+        System.out.print("| ");
         for (int j = 0; j < matrix[0].length; j++) {
             System.out.print(matrix[i][j] + " ");
         }
+        System.out.print("|");
         System.out.println();
     }   
 }
